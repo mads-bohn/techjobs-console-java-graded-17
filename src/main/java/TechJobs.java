e8577d407e8059f8228d118dce9058cd602f0ecf
mads-bohn <mady.bohn@gmail.com>
2024-04-12 17:03:37 -0500

    implemented printjobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -119,7 +119,15 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        //iterate over arraylist using for each loop
+        for (HashMap job : someJobs) {
+            System.out.println();
+            System.out.println("*****");
+            // iterate over hashmap using loop
+            job.forEach(
+                    (key, value)
+                            -> System.out.println(key + ": " + value));
+            System.out.println("*****");
+        }
     }
 }
