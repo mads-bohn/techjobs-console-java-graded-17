87997c66f1ad0ecc506953f18a8a17caf56a877f
mads-bohn <mady.bohn@gmail.com>
2024-04-19 12:17:47 -0500

    alphabetized list results

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -43,6 +40,7 @@ public class TechJobs {
                 } else {
 
                     ArrayList<String> results = JobData.findAll(columnChoice);
+                    Collections.sort(results);
 
                     System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
 
@@ -119,7 +117,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (someJobs.size() == 0) {
+        if (someJobs.isEmpty()) {
             System.out.print("No Results");
         }
 
